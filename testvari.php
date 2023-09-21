986fcaa6334057187e1304705d712fc03530448b
ninguemtapuro <130668995+ninguemtapuro@users.noreply.github.com>
2023-09-21 09:56:34 -0300

    commit

--- testvari.php
+++ testvari.php
@@ -1,9 +1,21 @@
 <?php
-    $num = 23.8;
+    $num = 23.7;
     if(is_int($num)){
         echo "Numero inteiro";
     }
         else {
             echo "O valor da variavel não é um numero inteiro!";
         }
+?>
+        <?php
+
+        $num = 23.7;
+        if(is_integer($num)){
+            echo "Numero inteiro";
+        }
+            else {
+                echo "O valor da variavel não é um numero inteiro!";
+            }
+
+
 ?>
