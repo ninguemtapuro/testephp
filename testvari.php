0694e59bb7ff6c5de0b6710901c383101aa12b67
ninguemtapuro <130668995+ninguemtapuro@users.noreply.github.com>
2023-09-21 10:39:34 -0300

    commit mig

--- testvari.php
+++ testvari.php
@@ -1,21 +1,79 @@
 <?php
-    $num = 23.7;
-    if(is_int($num)){
-        echo "Numero inteiro";
-    }
-        else {
-            echo "O valor da variavel não é um numero inteiro!";
-        }
-?>
-        <?php
-
-        $num = 23.7;
-        if(is_integer($num)){
-            echo "Numero inteiro";
-        }
-            else {
-                echo "O valor da variavel não é um numero inteiro!";
-            }
+//     $num = 23.7;
+//     if(is_int($num)){
+//         echo "Numero inteiro";
+//     }
+//         else {
+//             echo "O valor da variavel não é um numero inteiro!";
+//         }
 
 
-?>
+    
+
+//     $num = 23;
+//         if(is_integer($num)){
+//             echo "Numero inteiro";
+//         }
+//             else {
+//                 echo "O valor da variavel não é um numero inteiro!";
+//             }
+
+
+
+//     $num = 23;
+//             if(is_real($num)){
+//                 echo "Numero inteiro";
+//             }
+//                 else {
+//                     echo "O valor da variavel não é um numero inteiro!";
+//                 }        
+            
+
+
+//     $num = 23;
+//         if(is_long($num)){
+//             echo "Numero inteiro";
+//         }
+//             else {
+//                 echo "O valor da variavel não é um numero inteiro!";
+//             }
+
+
+//     $num = 23;
+//         if(is_float($num)){
+//             echo "Numero inteiro";
+//         }
+//             else {
+//                 echo "O valor da variavel não é um numero inteiro!";
+//             }
+
+
+//     $num = 23;
+//         if(is_string($num)){
+//             echo "Numero inteiro";
+//         }
+//             else {
+//                 echo "O valor da variavel não é um numero inteiro!";
+//             }
+
+
+
+//     $num = 23;
+//         if(is_array($num)){
+//             echo "Numero inteiro";
+//         }
+//             else {
+//                 echo "O valor da variavel não é um numero inteiro!";
+//             }
+
+
+
+//     $num = 23;
+//         if(is_object($num)){
+//             echo "Numero inteiro";
+//         }
+//             else {
+//                 echo "O valor da variavel não é um numero inteiro!";
+//             }
+
+// ?>
