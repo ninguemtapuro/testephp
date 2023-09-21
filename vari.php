da396b3779ca2df8c6968059398f86cba49dd35b
ninguemtapuro <130668995+ninguemtapuro@users.noreply.github.com>
2023-09-21 09:37:01 -0300

    commit mig

---
+++ vari.php
@@ -0,0 +1,17 @@
+<?php
+    // variavel do tipo String
+    $none = 'Meu nome 123';
+    echo "none";
+
+    //variavel do tipo inteiro
+    $ano = 2023;
+    echo "$ano";
+
+    //variavel do tipo float
+    $pi = 3.3445434534;
+    echo "$pi";
+    //variavel do tipo verdadeiro
+    $sim = true;
+    echo "$sim";
+    
+?>
