d6cc11b2f4d808d9cf7c647a95943aef1a58394f
ninguemtapuro <130668995+ninguemtapuro@users.noreply.github.com>
2023-09-29 13:39:10 -0300

    commit

---
+++ atv3.php
@@ -0,0 +1,15 @@
+<?php 
+    //Declaração de variáveis
+    $valor1 = 41;
+    $valor2 = 50;
+    $valor3 = 40;
+    $valor4 = 10000;
+
+    if ($valor3 <= $valor1) {
+        echo "atenção";
+    }else if ($valor1 >= $valor2){ 
+        echo "Velocidade correta";
+    }else if ($valor2 > $valor4){ 
+        echo "Risco de multa";
+    }
+?>
