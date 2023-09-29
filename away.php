a0e5ac9524e25dab6f331d0d1db6b7835cd987d5
ninguemtapuro <130668995+ninguemtapuro@users.noreply.github.com>
2023-09-29 16:31:39 -0300

    commit

---
+++ away.php
@@ -0,0 +1,13 @@
+<?php
+$a = 10;
+
+    while($a < 30){
+
+        $a+=10;
+        echo "<br>";
+        echo "$a";
+        if($a == 30 || $a == 40){
+            echo "Pulou a execulção $a <br>";
+        }
+    }
+    ?>
