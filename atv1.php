4cb1060c1323e6e0fb4ea200cbd9525367712097
ninguemtapuro <130668995+ninguemtapuro@users.noreply.github.com>
2023-09-29 09:12:04 -0300

    commit

--- atv1.php
+++ atv1.php
@@ -8,14 +8,19 @@ $num4 = 11;
 
 if ($num1 > $num2) { 
     echo "5 maiorque 2";
+}else if ($num1 < $num2){ 
+    echo "2 não é maior que 5";
 }
 
 if ($Rob != $Ped) { 
     echo "Robson é diferente de Pedro";
+
 }
 
 if ($num3 <=  $num4) { 
     echo "12 é menor ou igual a 11";
+}else if ($num3 >= $num4){ 
+    echo "Erro";
 }
 ?>
 
